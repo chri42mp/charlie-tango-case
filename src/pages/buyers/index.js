8936876b12268b613280b9ad36d27a72b4b23ab3
Christine Ramm <chri42mp@stud.kea.dk>
2023-04-25 15:42:13 +0200

    fetch changed local host

--- src/pages/buyers/index.js
+++ src/pages/buyers/index.js
@@ -5,7 +5,7 @@ import styles from "./Buyers.module.css";
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(`http://localhost:3001/api/find-buyers`);
+  const res = await fetch(`http://localhost:3000/api/find-buyers`);
   const data = await res.json();
 
   // Pass data to the page via props
