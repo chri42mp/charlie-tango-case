b90650bbbd8495f2eb27195d0318b55047379188
salmamraih <salma.mraih@gmail.com>
2023-04-27 14:18:19 +0200

    possible contact form

---
+++ src/components/Contactform.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    if (response.ok) {
+      console.log("Message sent successfully");
+    } else {
+      console.error("Error sending message:", response.statusText);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone:</label>
+        <textarea
+          id="phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        ></textarea>
+      </div>
+      <button type="submit">Contact Buyers</button>
+    </form>
+  );
+};
+
+export default ContactForm;
+
