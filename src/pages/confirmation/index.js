1419bb68c8033c96eb5fb82663a4cb132a565a69
Mads Schou Olesen <mschouolesen@gmail.com>
2023-05-01 10:51:00 +0200

    confirmationpage

---
+++ src/pages/confirmation/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Confirmation() {
+  return (
+    <>
+      <Head>
+        <title>Confirmation | EDC</title>
+      </Head>
+
+      <div className="wrapper">
+        <h1>Thanks for contacting us!</h1>
+        <p>Our team will be in touch with you shortly.</p>
+      </div>
+    </>
+  );
+}
